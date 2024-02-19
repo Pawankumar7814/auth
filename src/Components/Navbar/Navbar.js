879e0a6401621ebe0a72b2808ce7280cc65b5326
Pawankumar7814 <pawank4285@gmail.com>
2024-02-19 22:33:29 +0530

    React hooks 1) Use State and 2) Use efffect

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,8 +1,20 @@
+import { useState, useEffect } from "react";
+
 function Navbar(props) {
+    // Declaring the variable in react
+    const [text,setText] = useState("This is navigation bar's Variable");
+
+    setTimeout(()=>{
+        setText("This is new navigation Bar which is changed");
+    },3000);
+    useEffect(function(){
+            console.log("The text has been changed");
+    },[text]);
     return (
         <div className="App" >
-        This is Navigation Bar's Component
-            <h1>{props.list}</h1>
+            {/* This is Navigation Bar's Component
+            <h1>{props.list}</h1> */}
+            {text}
         </div>
     );
 }
